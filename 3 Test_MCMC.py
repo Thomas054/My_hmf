d6fa1dc031c2a4cf18157d1c0bedc3fa15cd4e40
Achard Thomas <thomas.achard@student-cs.fr>
2024-09-26 16:48:11 +0200

    Nouvelle nomenclature

--- 3 Test_MCMC.py
+++ 3 Test_MCMC.py
@@ -27,26 +27,31 @@ def MCMC(computedpars,N,stepfactor,thetai,Ncamb,plot,add=True):
     """
     
     # car = f"{N}__{stepfactor}__{thetai[0]}__{thetai[1]}__{Ncamb}"
-    car = f"*__{stepfactor}__"
+    car = f"{stepfactor}__"
     for par in thetai:
         car += f"{par}__"
     if add:
         car += "add"
     
-    files_pars = glob.glob(f'data/{car}__pars.csv')
-    files_chi2 = glob.glob(f'data/{car}__chi2.csv')
-    if add and len(files_pars) != 0 and len(files_chi2) != 0:       # "add" dans le nom du fichier indique si on a le droit d'ajouter des choses ou pas
-        print("file found")
-        numeros = [int(re.findall(r'\d+', file)[0]) for file in files_pars]     # On récupère les premiers numéros dans les noms de fichiers existants
+    # files_pars = glob.glob(f'data/{car}__pars.csv')
+    # files_chi2 = glob.glob(f'data/{car}__chi2.csv')
+    # if add and len(files_pars) != 0 and len(files_chi2) != 0:       # "add" dans le nom du fichier indique si on a le droit d'ajouter des choses ou pas
+    #     print("file found")
+    #     numeros = [int(re.findall(r'\d+', file)[0]) for file in files_pars]     # On récupère les premiers numéros dans les noms de fichiers existants
         
-        L_pars_prec = np.loadtxt(files_pars[np.argmax(numeros)])
-        L_chi2_prec = np.loadtxt(files_chi2[np.argmax(numeros)])
-        # Bref, on récupère les données avec le plus d'itérations
-        # Puis on supprime les autres
-        for i in range(len(files_pars)):
-            if i != np.argmax(numeros):
-                os.remove(files_pars[i])
-                os.remove(files_chi2[i])
+    #     L_pars_prec = np.loadtxt(files_pars[np.argmax(numeros)])
+    #     L_chi2_prec = np.loadtxt(files_chi2[np.argmax(numeros)])
+    #     # Bref, on récupère les données avec le plus d'itérations
+    #     # Puis on supprime les autres
+    #     for i in range(len(files_pars)):
+    #         if i != np.argmax(numeros):
+    #             os.remove(files_pars[i])
+    #             os.remove(files_chi2[i])
+    if add and os.path.exists(f'data/{car}__pars.csv') and os.path.exists(f'data/{car}__chi2.csv'):
+        L_pars_prec = np.loadtxt(f'data/{car}__pars.csv')
+        L_chi2_prec = np.loadtxt(f'data/{car}__chi2.csv')
+    
+    
     else:
         L_pars_prec = None
         L_chi2_prec = None
@@ -61,7 +66,7 @@ def MCMC(computedpars,N,stepfactor,thetai,Ncamb,plot,add=True):
     L_pars, L_chi2 = s.calc_params(thetai, N, step, plot, L_pars_prec=L_pars_prec, L_chi2_prec=L_chi2_prec)
 
 
-    car = f"{len(L_chi2)}__{stepfactor}__"        # On stocke dans un nouveau fichier vu qu'on a plus d'itérations
+    car = f"{stepfactor}__"        # On stocke dans un nouveau fichier vu qu'on a plus d'itérations
     for par in thetai:
         car += f"{par}__"
     if add:
